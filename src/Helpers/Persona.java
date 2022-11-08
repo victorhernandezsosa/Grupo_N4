804684817429ba15dd227d54377406b0443bbbf1
victorhernandezsosa <victorhernadez093@gmail.com>
2022-11-08 11:47:03 -0600

    SuperClase Persona Avance 2

--- src/Helpers/Persona.java
+++ src/Helpers/Persona.java
@@ -9,5 +9,48 @@ package Helpers;
  * @author ASUS
  */
 public class Persona {
+     private int Edad;
+     private String Nombre;
+     private String Apellido;
+     private String Genero;
+     
+     public Persona(){
     
 }
+     public void setEdad(int _edad){
+         this.Edad=_edad;
+     }
+     
+     public void setNombre(String _nombre){
+         this.Nombre=_nombre;
+     }
+     
+     public void setApellido(String _apellido){
+         this.Apellido=_apellido;
+     }
+     
+     public void setGenero(String _genero){
+         this.Genero=_genero;
+     }
+     
+     public int getEdad(){
+         return this.Edad;
+     }
+     
+     public String getNombre(){
+         return this.Nombre;
+     }
+     
+     public String getApellido(){
+         return this.Apellido;
+     }
+     
+     public String getGenero(){
+         return this.Genero;
+     }
+     
+     
+     
+}
+
+
