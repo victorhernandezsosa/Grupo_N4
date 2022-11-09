7e22d64f26ead52d5ae6497189362e74fbd3ba78
Shameya <117786536+Shameya@users.noreply.github.com>
2022-11-09 17:59:11 -0600

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Doctor extends Persona {
+
+    public Doctor() {
+    setEdad("18");
+    setNombre("Princess");
+    setApellido("Smith)";
+    setGenero("Femenino");
+    }
+   
+}
